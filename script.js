4a75800abcf6e079dc0be66df0448efdeb46059c
kubilaikhan06 <tld3906@hotmail.com>
2018-06-30 07:04:20 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+function dessiner(){
+    var rows= document.getElementById('rows').value;
+    var cols= document.getElementById('cols').value;
+    var ecran = document.getElementById('ecran');
+    ecran.innerHTML = makeTable(rows, cols);
+
+}
+var color = 'blue';
+function makeTable(r, c){
+    var table = "<table border='1' >";
+    for (var i = 0; i<r ; i++){
+        table+= "<tr>";
+        for (var j=0; j<c; j++){
+            //table += "<td>"+i+","+j+"</td>";
+
+            table+= "<td onmouseover='colorer(this, event)'></td>";
+        }
+        table+= "</tr>";
+    }
+    
+    table += "</table>";
+    return table;
+}
+function colorer(td, evt){
+    td.style.backgroundColor= color;
+    console.log(evt);
+}
